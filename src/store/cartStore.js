3d71c6258aaace52db3104e765a857e8c9b24b92
Daniel Pineda <daniel.pinedag@upb.edu.co>
2026-04-19 18:02:24 -0500

    feat: setup cart store with zustand persistence

---
+++ src/store/cartStore.js
@@ -0,0 +1,28 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+export const useCartStore = create(
+  persist(
+    (set, get) => ({
+      cart: [],
+      addToCart: (product) => {
+        const currentCart = get().cart;
+        const existingItem = currentCart.find(item => item.id === product.id);
+
+        if (existingItem) {
+          set({
+            cart: currentCart.map(item =>
+              item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+            ),
+          });
+        } else {
+          set({ cart: [...currentCart, { ...product, quantity: 1 }] });
+        }
+      },
+      removeFromCart: (id) => set({ cart: get().cart.filter(item => item.id !== id) }),
+      clearCart: () => set({ cart: [] }),
+      getTotal: () => get().cart.reduce((acc, item) => acc + (item.price * item.quantity), 0),
+    }),
+    { name: 'shopping-cart' } // Nombre para localStorage
+  )
+);
