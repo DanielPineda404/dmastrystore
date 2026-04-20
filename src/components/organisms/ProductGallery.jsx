58106cef5ccd9f33e0e00bfb79c60dc70843ee8b
Daniel Pineda <daniel.pinedag@upb.edu.co>
2026-04-19 19:59:18 -0500

    feat: add loading state to ProductGallery

--- src/components/organisms/ProductGallery.jsx
+++ src/components/organisms/ProductGallery.jsx
@@ -2,6 +2,7 @@ import { ProductCard } from "../molecules/ProductCard.jsx";
 import { useCartStore } from "../../store/cartStore.js";
 import { useProductStore } from "../../store/productStore.js";
 import { Pagination } from "../molecules/Pagination.jsx";
+import { Loader2 } from "lucide-react"; // Icono de carga
 
 export const ProductGallery = () => {
   const addToCart = useCartStore((state) => state.addToCart);
@@ -9,11 +10,21 @@ export const ProductGallery = () => {
     getPaginatedProducts, 
     getTotalPages, 
     currentPage, 
-    setCurrentPage 
+    setCurrentPage,
+    isLoading 
   } = useProductStore();
 
   const productsToShow = getPaginatedProducts();
   const totalPages = getTotalPages();
+
+  if (isLoading) {
+    return (
+      <div className="py-20 flex flex-col items-center justify-center gap-4">
+        <Loader2 className="animate-spin text-zinc-300" size={40} />
+        <p className="text-zinc-400 font-medium">Loading products...</p>
+      </div>
+    );
+  }
 
   if (productsToShow.length === 0) {
     return (
